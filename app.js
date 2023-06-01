efd12e78c1a9b1821034bbca3f21e52f323e9775
PareshHK <phatikankan@gmail.com>
2023-06-01 21:57:31 +0530

    done

--- app.js
+++ app.js
@@ -2,12 +2,36 @@ var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
 var outputDiv = document.querySelector("#output");
 
-console.log(txtInput)
+// var serverURL ="https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+
+var serverURL ="https://api.funtranslations.com/translate/hodor.json"
+
+
+function getTranslationURL(input){
+    return serverURL + "?" + "texts" +input
+}
+
+ function errorHandler(error){
+     console.log("error occured", error);
+     alert("something wrongwith the server")}
+ 
+
+
 
 
 function clickHandler() {
 
-        outputDiv.innerText="jdhsbejdjjl " + txtInput.value;
+        var inputText = txtInput.value;
+
+        fetch(getTranslationURL(inputText))
+        .then(response => response.json())
+        .then(json => {
+            var translatedText =json.contents.translated;
+            outputDiv.innerText = translatedText;
+            
+        })
+         .catch(errorHandler)
+
         
         
 
